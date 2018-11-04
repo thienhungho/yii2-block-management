09ea993f11e7a83eefb77d3ac5531d6b3a05519a
Harry Ho <thienhungho@gmail.com>
2018-11-04 20:36:00 +0700

    Fix BlockSearch

--- src/modules/BlockManage/search/BlockSearch.php
+++ src/modules/BlockManage/search/BlockSearch.php
@@ -5,7 +5,7 @@ namespace thienhungho\Block\modules\BlockManage\search;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\modules\blocks\Block;
+use thienhungho\Block\modules\BlockBase\Block;
 
 /**
  * thienhungho\Block\modules\BlockManage\search\BlockSearch represents the model behind the search form about `common\modules\blocks\Block`.
